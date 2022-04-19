8aebf210315012f3dbd50dd16262bdf37e663642
CaptainSobeKing <57887816+CaptainSobeKing@users.noreply.github.com>
2022-04-19 16:13:56 -0400

    Create make-ISO-OSX11.6.5.sh

---
+++ make-ISO-OSX11.6.5.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#===========================================================================
+# Works only with the official image available in the Mac App Store.
+# Make sure you download the official installer before running this script.
+#===========================================================================
+
+# Change this at your desire. Sometimes this works out of the box, sometimes not.
+# Default size: ~13.5 GB
+DISK_SIZE="13000m"
+
+#===========================================================================
+
+hdiutil create -o /tmp/BigSur.cdr -size $DISK_SIZE -layout SPUD -fs HFS+J
+
+hdiutil attach /tmp/BigSur.cdr.dmg -noverify -mountpoint /Volumes/install_build
+
+sudo "/Applications/Install macOS Big Sur.app/Contents/Resources/createinstallmedia"  --volume /Volumes/install_build --nointeraction
+
+hdiutil detach "/Volumes/Shared Support 1"
+
+hdiutil detach "/Volumes/Shared Support"
+
+hdiutil detach "/Volumes/Install macOS Big Sur"
+
+hdiutil convert /tmp/BigSur.cdr.dmg -format UDTO -o /tmp/BigSur.iso
+
+mv /tmp/BigSur.iso.cdr ~/Desktop/BigSur.iso
+
+rm /tmp/BigSur.cdr.dmg
