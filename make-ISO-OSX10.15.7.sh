7d73ebd6d8ac97c875dc50e7c5a8a48ac47d8b20
CaptainSobeKing <57887816+CaptainSobeKing@users.noreply.github.com>
2022-04-19 15:58:54 -0400

    Update make-ISO-OSX10.15.7.sh

--- make-ISO-OSX10.15.7.sh
+++ make-ISO-OSX10.15.7.sh
@@ -4,10 +4,23 @@
 # Make sure you download the official installer before running this script.
 #===========================================================================
 
+# Create a DMG Disk Image
 hdiutil create -o /tmp/Catalina.cdr -size 9000m -layout SPUD -fs HFS+J
+
+# Mount it to your macOS
 hdiutil attach /tmp/Catalina.cdr.dmg -noverify -mountpoint /Volumes/install_build
+
+# Create macOS Catalina Installer
 sudo /Applications/Install\ macOS\ Catalina.app/Contents/Resources/createinstallmedia  --volume /Volumes/install_build --nointeraction
+
+# Unmount Catalina Disk
 hdiutil detach "/Volumes/Install macOS Catalina"
+
+# Convert the DMG file to an ISO file
 hdiutil convert /tmp/Catalina.cdr.dmg -format UDTO -o /tmp/Catalina.iso
+
+# Rename and Move to Desktop
 mv /tmp/Catalina.iso.cdr ~/Desktop/Catalina.iso
+
+# Remove temp .dmg
 rm /tmp/Catalina.cdr.dmg
